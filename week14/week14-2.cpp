4d77752f0c81fedb250a67f610e54befb3c761f4
YL-Aerial <oliver921113@gmail.com>
2022-12-06 11:59:16 +0800

    week13

---
+++ week14/week14-2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int z=10;
+void func()
+{
+    int y;
+    printf("剛進來func()時， z是%d\n",z);
+    z=2;
+    printf("在func()裡 ,把Z改成%d\n",z);
+}
+int main()
+{
+    int x;
+    func();
+    printf("在main()裡,z的值是%d\n",z);
+    z=1;
+    printf("在main()裡改 z的值現在是%d\n",z);
+}
